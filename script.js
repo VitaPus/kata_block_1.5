a569a45a077d10cd039be26101b5e3ea3fdf5d46
VitaPus <119700754+VitaPus@users.noreply.github.com>
2024-09-15 22:29:32 +0600

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-new Swiper('.swiper', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+let swiper = new Swiper('.swiper', {
+  direction: "horizontal",
+  spaceBetween: 15,
+  slidesPerView: "auto",
+  breakpoints: {
+    768: {
+      enabled: false,
     },
-
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });

746ef0ab687b18c6a546d2067392b59f94802962
VitaPus <119700754+VitaPus@users.noreply.github.com>
2024-09-19 00:42:08 +0600

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let swiper = new Swiper('.swiper', {
   direction: "horizontal",
   spaceBetween: 15,
-  slidesPerView: "auto",
+  slidesPerView: 1.3,
   breakpoints: {
     768: {
       enabled: false,
@@ -11,3 +11,24 @@ let swiper = new Swiper('.swiper', {
     el: ".swiper-pagination",
   },
 });
+
+let hideBtn = function () {
+  let clickkBtn = document.querySelector('.btn-slide--vision');
+  let hideSlide = document.querySelectorAll('.hidden, .swiper-slide--hidden');
+  if (clickkBtn) {
+    clickkBtn.addEventListener('click', function () {
+      clickkBtn.classList.toggle('active');
+      hideSlide.forEach(slide => {
+        slide.classList.toggle('hidden');
+        slide.classList.toggle('swiper-slide--hidden');
+      });
+      if (clickkBtn.classList.contains('active')) {
+        clickkBtn.textContent = 'Скрыть';
+      } else {
+        clickkBtn.textContent = 'Показать все';
+      }
+    });
+  }
+};
+
+hideBtn();
